f2cb34ff4f82a2b8941ae207d40f4ec1ffb6f881
Cat <madcatmac16@gmail.com>
2018-11-04 20:47:54 -0700

    js, css, exphbs

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bluecheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('American', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Onionrings', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroomswiss', false);
